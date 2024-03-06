c081782c2cc6918647e2799ce71511db737dc4b5
Erik Guzman <erik.guzman@guzman.codes>
2024-03-06 04:27:18 +0000

    Remove old files and update build configs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,9 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable import/no-extraneous-dependencies */
 import react from '@vitejs/plugin-react'
+import path, { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
@@ -8,7 +11,7 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.tsx'),
+      entry: resolve(path.dirname(fileURLToPath(import.meta.url)), 'src/index.tsx'),
       name: 'react-twitch-embed-video',
       // the proper extensions will be added
       fileName: 'react-twitch-embed-video',
