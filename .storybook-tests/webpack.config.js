bc30c6afdc754d15ae454a6cf00c20ccce9582dd
Erik Guzman <erik.guzman@guzman.codes>
2024-03-06 04:27:18 +0000

    Remove old files and update build configs

--- .storybook-tests/webpack.config.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-// your app's webpack.config.js
-const custom = require('../webpack.config.babel.js');
-
-module.exports = async ({ config, mode }) => {
-  return { ...config, watchOptions: custom.watchOptions };
-};
