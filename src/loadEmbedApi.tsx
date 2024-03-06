e5bae8f4e9696cff1cc10c0cb798fcc51120be85
Erik Guzman <erik.guzman@guzman.codes>
2024-03-06 05:00:42 +0000

    Update ESLint configs

--- src/loadEmbedApi.tsx
+++ src/loadEmbedApi.tsx
@@ -4,7 +4,9 @@ const func: () => unknown = () => {}
 
 const loadEmbedApi = (callback = func): void => {
   // Check if the script tag already exists
-  if(document.querySelector("script[src='https://embed.twitch.tv/embed/v1.js']"))
+  if (
+    document.querySelector("script[src='https://embed.twitch.tv/embed/v1.js']")
+  )
     return
 
   const script = document.createElement('script')
